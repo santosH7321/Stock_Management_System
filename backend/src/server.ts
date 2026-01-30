9b2eda334e9f93e4596eb6a3f66e9c46f41f1968
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-30 11:17:06 +0530

    Server and mongoose setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ import { connectDB } from "./config/db";
 
 connectDB();
 
-const PORT = process.env['PORT'] || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
