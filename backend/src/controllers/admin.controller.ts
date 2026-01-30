def2dae2e3ce76a113d99f690dd5e12b0c72b46c
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-30 12:03:22 +0530

    create Guard controller

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -24,3 +24,42 @@ export const createHostel = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const createGuard = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password, hostelId } = req.body;
+
+    const hostel = await Hostel.findById(hostelId);
+
+    if (!hostel)
+      return res.status(404).json({
+        message: "Hostel not found"
+      });
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser)
+      return res.status(400).json({
+        message: "User already exists"
+      });
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const guard = await User.create({
+      name,
+      email: email.toLowerCase(),
+      password: hashed,
+      role: "GUARD",
+      hostelId
+    });
+
+    res.status(201).json({
+      message: "Guard created successfully",
+      guard
+    });
+
+  } catch {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
