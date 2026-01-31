19a94c94e687a9258b0fd67a460690c8a1e991c5
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-31 23:16:59 +0530

    Login page

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useState } from "react";
+import api from "@/lib/axios";
+import { useRouter } from "next/navigation";
+
+export default function Login() {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await api.post("/auth/login", { email, password });
+
+    localStorage.setItem("token", res.data.token);
+
+    if (res.data.user.role === "ADMIN") router.push("/admin");
+    else router.push("/guard");
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-indigo-100 via-white to-blue-100 px-4">
+        <div className="w-full max-w-md">
+            <div className="backdrop-blur-xl bg-white/80 shadow-2xl rounded-3xl p-8 space-y-6 border border-white/40">
+
+            <div className="text-center space-y-2">
+                <h1 className="text-3xl font-bold text-gray-800">Welcome to GECJ 👋</h1>
+                <p className="text-gray-500 text-sm">Login to continue your journey</p>
+            </div>
+
+            <input
+                type="email"
+                placeholder="Email address"
+                onChange={(e) => setEmail(e.target.value)}
+                className="
+                w-full px-4 py-3 rounded-xl
+                border border-gray-200
+                bg-white text-gray-600
+                focus:outline-none focus:ring-2 focus:ring-indigo-500
+                transition-all duration-200
+                "
+            />
+
+            <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                className="
+                w-full px-4 py-3 rounded-xl
+                border border-gray-200
+                bg-white text-gray-600
+                focus:outline-none focus:ring-2 focus:ring-indigo-500
+                transition-all duration-200
+                "
+            />
+
+            <button
+                onClick={handleLogin}
+                className="
+                w-full py-3 rounded-xl
+                bg-indigo-600 hover:bg-indigo-700
+                text-white font-semibold
+                shadow-lg hover:shadow-xl
+                transition-all duration-200 active:scale-95
+                "
+            >
+                Login
+            </button>
+
+            </div>
+        </div>
+    </div>
+
+  );
+}
