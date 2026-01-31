2f6e2982b18ade7ec91ceadc6a2792556b01d8dc
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-31 22:35:09 +0530

    helmet and express rate limit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,11 +5,22 @@ import adminRoutes from "./routes/admin.routes";
 import itemRoutes from "./routes/item.routes";
 import stockRoutes from "./routes/stock.routes";
 import logRoutes from "./routes/log.routes";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 200
+  })
+);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/items", itemRoutes);
